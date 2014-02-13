a48bf09d940f9398653495c628cb25b208c3b701
Anand S Katti <anand.katti@radisys.com>
2014-02-13 18:12:18 +0530

    merged

--- test.c
+++ test.c
@@ -4,10 +4,6 @@ main ()
 printf ("Hello Git \n");
 printf ("Test merge\n");
 printf ("ReTest merge\n");
-<<<<<<< HEAD
 printf (" This is master branch\n");
-=======
 printf ("Adding new feature to this file on a branch 'newfeature' \n");
-
->>>>>>> newfeature
 }
