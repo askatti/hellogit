833d42689ba861a033e1641be185a89eafb3f2f9
Anand S Katti <anand.katti@radisys.com>
2014-02-13 18:13:27 +0530

    udpate from new branch

--- test.c
+++ test.c
@@ -5,5 +5,5 @@ printf ("Hello Git \n");
 printf ("Test merge\n");
 printf ("ReTest merge\n");
 printf ("Adding new feature to this file on a branch 'newfeature' \n");
-
+printf (" This is update from newfeature branch\n");
 }
